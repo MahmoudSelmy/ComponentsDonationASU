7758cfc416788c4c55063caea4ae737433c10747
MahmoudSelmy <mahmoudselmy06@gmail.com>
2017-04-26 03:32:35 +0200

    permit StrictMode

--- app/src/main/java/com/sheatouk/selmy/componentsdonationasu/MainActivity.java
+++ app/src/main/java/com/sheatouk/selmy/componentsdonationasu/MainActivity.java
@@ -1,7 +1,11 @@
 package com.sheatouk.selmy.componentsdonationasu;
 
+import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.sheatouk.selmy.componentsdonationasu.Services.ScrapCompService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // to permit strictMode
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        startService(new Intent(this , ScrapCompService.class));
     }
 }
